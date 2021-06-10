647a6d7b4bbd11e7fb84dc6ebd48aa7e99a8ae37
Dan James <danjameshh@hotmail.com>
2021-06-10 15:18:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import Feed from "./Components/Feed/Feed";
 import Header from "./Components/Header/Header";
+import Login from "./Components/Login/Login";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { login, logout, selectUser } from "./features/counter/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { auth } from "./Components/Firebase/firebase";
+import Widgets from "./Components/Widgets/Widgets";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //user is logged in
+        dispatch(
+          login({
+            email: userAuth.user.email,
+            uid: userAuth.user.uid,
+            displayName: userAuth.displayName,
+            photoURL: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <>
       <div className="bg-linkedingrey flex flex-col  ">
         <Header />
-        <div className="flex ">
-          <Sidebar />
-          <Feed />
-        </div>
+
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="flex justify-center gap-x-9 ">
+            <Sidebar />
+            <Feed />
+          </div>
+        )}
       </div>
     </>
   );
