eab1828a73ec4e8933ff14e264af7647f2ec28fc
Dan James <danjameshh@hotmail.com>
2021-06-10 15:19:28 +0100

    Update HeaderItem.js

--- src/Components/Header/HeaderItem.js
+++ src/Components/Header/HeaderItem.js
@@ -1,10 +1,13 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 
-function HeaderItem({ title, Icon, avatar }) {
+function HeaderItem({ title, Icon, avatar, onClick }) {
   return (
     <>
-      <div className="flex flex-col items-center cursor-pointer w-12 text-gray-500 text-opacity-70 sm:w-20 hover:text-gray-700 ">
+      <div
+        onClick={onClick}
+        className="flex flex-col items-center cursor-pointer w-12 text-gray-500 text-opacity-70 sm:w-20 hover:text-gray-700 "
+      >
         {Icon && <Icon className="h-5 w-5" />}
         {avatar && <Avatar src={avatar} className="object-contain" />}
         <p className="tracking-tight whitespace-nowrap text-xs ">{title}</p>
